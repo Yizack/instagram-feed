af27dea180056e844204d3c16ba8689705afe854
Yizack Rangel <yizackr@gmail.com>
2020-03-08 16:50:10 -0500

    font awesome on head

--- feed.php
+++ feed.php
@@ -10,6 +10,7 @@ $post = instagramFeed();
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="<?= fontawesome(); ?>" crossorigin="anonymous"></script>
   </head>
   <style>
     html {
@@ -69,7 +70,6 @@ $post = instagramFeed();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="<?= fontawesome(); ?>" crossorigin="anonymous"></script>
     <script>
       let n = 0;
       function next(){
